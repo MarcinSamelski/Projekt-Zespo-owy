c05d9a3088eefeffe0ae675b555e28942b73f4f1
czerwonogrodzki <czerwonogrodzkipatryk@gmail.com>
2024-04-24 18:13:40 +0200

    Zmiany w gitignore, dodanie obsługę bazy sqlite poprzez sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sqlite = require("sqlite3");
 
 const app = express();
 const port = 3000;
 
-// Dane przykładowej bazy danych
-const users = [
-    { id: 1, username: 'jan_kowalski', password: 'haslo123', balance: 1000 },
-    { id: 2, username: 'anna_nowak', password: 'test123', balance: 500 }
-];
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./clients.sqlite",
+});
+
+class Client extends Model {}
+Client.init(
+  {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    PESEL: DataTypes.STRING,
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    accountNumber: DataTypes.STRING,
+    balance: DataTypes.DECIMAL,
+  },
+  { sequelize, modelName: "client" }
+);
+
+sequelize.sync().then(
+  async () =>
+    await Client.findOrCreate({
+      where: {
+        username: "admin",
+        password: "admin",
+      },
+    })
+);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routing
-app.get('/', (req, res) => {
-    res.send('Witaj w banku!');
+app.get("/", (req, res) => {
+  res.send("Witaj w banku!");
 });
 
 // Zakładanie nowego konta
-app.post('/zarejestruj', (req, res) => {
-    const { username, password } = req.body;
-    const newUser = { id: users.length + 1, username, password, balance: 0 };
-    users.push(newUser);
-    res.send(`Konto dla użytkownika ${username} zostało pomyślnie utworzone.`);
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  const newUser = { id: users.length + 1, username, password, balance: 0 };
+  users.push(newUser);
+  res.send(`Konto dla użytkownika ${username} zostało pomyślnie utworzone.`);
 });
 
 // Logowanie i sprawdzanie salda
-app.post('/zaloguj', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-        res.send(`Witaj ${username}, twoje saldo wynosi ${user.balance} zł.`);
-    } else {
-        res.status(401).send('Nieprawidłowe dane logowania.');
-    }
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (user) {
+    res.send(`Witaj ${username}, twoje saldo wynosi ${user.balance} zł.`);
+  } else {
+    res.status(401).send("Nieprawidłowe dane logowania.");
+  }
 });
 
 // Nasłuch
 app.listen(port, () => {
-    console.log(`Serwer bankowy działa na porcie ${port}`);
+  console.log(`Serwer bankowy działa na porcie ${port}\n`);
 });
